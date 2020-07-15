881f5de23813c9457da4a9c0f2000bcbf9772d6f
Cheng Hu <williamcheng.hu@gmail.com>
2020-07-15 17:45:24 +0300

    part2  courseInfo (part1) step6

--- part1/courseInfo/README.md
+++ part1/courseInfo/README.md
@@ -1,6 +1,6 @@
-*This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).*
+_This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app)._
 
-## Exercises 1.1.-1.2.
+## Exercises 1.1.-1.2. & 2.1.-2.5.
 
 Run the exercise with:
 
@@ -11,10 +11,30 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 ## Step 1
 
-Break component *App* into three new components: *Header*, *Content*, and *Total*.
+Break component _App_ into three new components: _Header_, _Content_, and _Total_.
 
-All data still resides in the *App* component, which passes the necessary data to each component using *props*.
+All data still resides in the _App_ component, which passes the necessary data to each component using _props_.
 
 ## Step 2
 
-Refactor the *Content* component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders three *Part* components of which each renders the name and number of exercises of one part.
+Refactor the _Content_ component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders three _Part_ components of which each renders the name and number of exercises of one part.
+
+## Step 6
+
+Refactor the _App_ component, define a component responsible for formatting a single course called _Course_.
+
+The component structure of the application can be, for example, the following:
+
+    App
+    Course
+        Header
+        Content
+        Part
+        Part
+        ...
+
+Hence, the _Course_ component contains the components defined in the previous part, which are responsible for rendering the course name and its parts.
+
+The application must work _regardless of the number of parts a course has_, so make sure the application works if you add or remove parts of a course.
+
+Ensure that the console shows no errors!
--- part1/courseInfo/src/index.js
+++ part1/courseInfo/src/index.js
@@ -1,71 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+/* const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
+  return <p>Number of exercises {sum}</p>;
+}; */
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      {parts.map((e) => (
+        <Part key={e.id} part={e} />
+      ))}
     </div>
   );
 };
 
-const Part = (props) => {
+const Course = ({ course }) => {
   return (
     <div>
-      <p>
-        {props.name} {props.exercises}
-      </p>
-    </div>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises{' '}
-        {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-      </p>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
     </div>
   );
 };
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
